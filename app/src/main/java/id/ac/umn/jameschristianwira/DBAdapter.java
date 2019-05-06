d57f2b30e4d26a8ace951074b306d560c903ae41
pottato22 <39824958+pottato22@users.noreply.github.com>
2019-05-06 22:29:41 +0700

    Update DBAdapter.java

--- app/src/main/java/id/ac/umn/jameschristianwira/DBAdapter.java
+++ app/src/main/java/id/ac/umn/jameschristianwira/DBAdapter.java
@@ -36,7 +36,6 @@ public class DBAdapter extends SQLiteOpenHelper {
 
     public SQLiteDatabase openDatabase(){
         Log.d(this.getClass().toString(), "openDatabase()");
-        Log.d(this.getClass().toString(), "openDatabase()");
         String path = DB_PATH + DB_NAME;
         if(db == null){
             createDatabase(path);
